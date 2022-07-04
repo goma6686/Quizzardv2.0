1cb1e0ff713180eb21b243213edc20a108f337e2
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2022-07-05 00:37:51 +0300

    svaresne forma

---
+++ resources/views/layouts/answers/true-false.blade.php
@@ -0,0 +1,4 @@
+<div class="form-check pt-4">
+    <label for="is_true">Is True?</label>
+    <input class="form-check-input" type="checkbox" value="" name="is_true">
+</div>
--- resources/views/layouts/create-question.blade.php
+++ resources/views/layouts/create-question.blade.php
@@ -5,6 +5,12 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h3 class="text-center p-3">Create Quiz Question</h3>
                     {{ Html::ul($errors->all()) }}
+                    <form enctype="multipart/form-data" method="POST" action="#">
+                        @csrf
+                    <div class="form-group pt-4 mb-4">
+                        <label>Question Text:</label>
+                        <input type="text" name="title" class="form-control" required="">
+                    </div>
                     <label>Choose Type:</label>
                     <br>
                     <select name="category" id="options" >
@@ -15,9 +21,23 @@
                     </select>
                     
                     <!-- gal divus galima labiau dynamic, idk xd -->
-                    <div id="content-1" class="content hidden"> @include('layouts.question-types.simple') </div>
-                    <div id="content-2" class="content hidden"> @include('layouts.question-types.multiple-choice') </div>
-                    <div id="content-3" class="content hidden"> @include('layouts.question-types.true-false') </div>
+                    <div id="content-1" class="content hidden"> @include('layouts.answers.answer') </div>
+                    <div id="content-2" class="content hidden"> @include('layouts.answers.multi-answer') </div>
+                    <div id="content-3" class="content hidden"> @include('layouts.answers.true-false') </div>
+                    <div class="form-group pt-4">
+                        <div class="col-md-2">
+                        <label for="category">Category:</label>
+                        <select class="form-control" name="category" type="category" required>
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}" selected > {{$category->name}} </option>
+                                @endforeach
+                        </select>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-secondary text-dark">Submit</button>
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
