0a4f7345689c332da3e3b5831c6a2a124a152b90
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2022-07-04 13:56:37 +0300

    + types table

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -10,4 +10,9 @@ class Answer extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
--- database/migrations/2022_07_04_104317_create_types_table.php
+++ database/migrations/2022_07_04_104317_create_types_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('type')->unique();
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('types');
     }
 };
