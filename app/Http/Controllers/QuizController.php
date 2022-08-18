a240778d28d909fd99412eef71f8c0e55a72a46d
lingegrino <77505391+lingegrino@users.noreply.github.com>
2022-08-18 13:50:45 +0300

    paginate questions v0.1

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -8,11 +8,13 @@ use App\Models\Category;
 use App\Models\Question;
 use App\Models\Answer;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Collection;
 
 class QuizController extends Controller
 {
     public function quiz(Request $request){
-        $questions = Question::with('answers')->inRandomOrder()->get();
+        $questions = Question::with('answers')->inRandomOrder()->simplePaginate(1);
         //return $question;
         return view('game.question', ['questions' => $questions]);
 
