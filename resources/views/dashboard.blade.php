5a9a653b97cdfdda92a79093ef7b9adde94be694
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2022-08-31 23:25:48 +0300

    grazinau jor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
     <div class="full position-relative">
         <div class="d-grid gap-2 col-6 mx-auto text-align: center">
             <a class="btn btn-outline-dark" type="button" href="/game">Arcade</a>
-            <a class="btn btn-outline-dark" type="button">Categories</a>
+            <a class="btn btn-outline-dark" type="button" href="/getseed">Custgom Seed</a>
+            <a class="btn btn-outline-dark" type="button" href="/getcategory">Categories</a>
         </div>
 </x-app-layout>
